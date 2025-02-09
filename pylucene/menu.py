e0ea40e5af12cd573a54e4aabfffea71049ad591
Kapput02 <eliacapiluppi02@gmail.com>
2025-02-09 22:20:25 +0100

    b

--- pylucene/menu.py
+++ pylucene/menu.py
@@ -22,11 +22,7 @@ import time
 # DONE
 fields = ["content", "book_title", "summary"]
 
-boost = {
-    "content": 1.0, 
-    "book_title": 2.0,
-    "summary": 1.5,   
-}
+boosts = [1.0, 2.0, 1.5]
 
 def modelUI():
     print("\n")
@@ -89,9 +85,7 @@ def calculate_ap(retrieved, relevant):
 def execute_queries(searcher, queries):
     interpolated_precisions = {}  # Salva precisioni interpolate per ogni query
     results_table = []
-    query_parser = MultiFieldQueryParser(["content", "book_title", "summary"], StandardAnalyzer())
-    for field, weight in boost.items():
-        query_parser.setBoost(field, weight)
+    query_parser = MultiFieldQueryParser(["content", "book_title", "summary"], StandardAnalyzer(),boosts)
     #query_parser = QueryParser("content", StandardAnalyzer())
 
     for query_text, relevant_docs in queries.items():
@@ -192,9 +186,7 @@ def compare_models(indexes_dir, benchmark_file):
         searcher = IndexSearcher(DirectoryReader.open(directory))
         searcher.setSimilarity(model)
         #query_parser = QueryParser("content", StandardAnalyzer())
-        query_parser = MultiFieldQueryParser(["content", "book_title", "summary"], StandardAnalyzer())
-        for field, weight in boost.items():
-            query_parser.setBoost(field, weight)
+        query_parser = MultiFieldQueryParser(["content", "book_title", "summary"], StandardAnalyzer(),boosts)
         for query_text, relevant_docs in queries.items():
             query = query_parser.parse(query_text)
             scoreDocs = searcher.search(query, 20).scoreDocs
@@ -246,9 +238,7 @@ def main():
     directory = NIOFSDirectory(Paths.get(os.path.join(indexes_dir, index_sub_dir)))
     searcher = IndexSearcher(DirectoryReader.open(directory))
     searcher.setSimilarity(model)
-    query_parser = MultiFieldQueryParser(["content", "book_title", "summary"], StandardAnalyzer())
-    for field, weight in boost.items():
-        query_parser.setBoost(field, weight)
+    query_parser = MultiFieldQueryParser(["content", "book_title", "summary"], StandardAnalyzer(),boosts)
     #query_parser = QueryParser("content", StandardAnalyzer())
     while True:
         # print("\nQUERY SYNTAX")
